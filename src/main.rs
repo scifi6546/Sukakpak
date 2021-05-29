58d387370b86becdc60d1c0cd59f875ff173a0ae
Nicholas Alexeev <nickalexeev@protonmail.com>
2021-05-29 11:12:46 -0800

    added cube

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ pub use ash::version::{DeviceV1_0, EntryV1_0, InstanceV1_0};
 mod graphics;
 
 use graphics::{Context, Vertex};
-use nalgebra::{Matrix4, Vector2, Vector3};
+use nalgebra::{Matrix4, Perspective3, Vector2, Vector3};
 
 use winit::{
     event::{Event, WindowEvent},
@@ -38,20 +38,82 @@ fn main() {
         ],
         vec![0, 1, 2],
     );
+    let cube = context.new_mesh(
+        textures[0],
+        vec![
+            Vertex {
+                position: Vector3::new(1.0, 0.0, 1.0),
+                uv: Vector2::new(1.0, 0.0),
+            },
+            Vertex {
+                position: Vector3::new(1.0, 0.0, 0.0),
+                uv: Vector2::new(1.0, 0.0),
+            },
+            Vertex {
+                position: Vector3::new(1.0, 1.0, 0.0),
+                uv: Vector2::new(1.0, 1.0),
+            },
+            Vertex {
+                position: Vector3::new(1.0, 1.0, 1.0),
+                uv: Vector2::new(1.0, 1.0),
+            },
+            Vertex {
+                position: Vector3::new(0.0, 0.0, 1.0),
+                uv: Vector2::new(0.0, 0.0),
+            },
+            Vertex {
+                position: Vector3::new(0.0, 0.0, 0.0),
+                uv: Vector2::new(0.0, 0.0),
+            },
+            Vertex {
+                position: Vector3::new(0.0, 1.0, 0.0),
+                uv: Vector2::new(0.0, 1.0),
+            },
+            Vertex {
+                position: Vector3::new(0.0, 1.0, 1.0),
+                uv: Vector2::new(0.0, 1.0),
+            },
+        ],
+        vec![
+            [0, 2, 1],
+            [0, 3, 2],
+            [4, 3, 0],
+            [4, 7, 3],
+            [3, 6, 2],
+            [3, 7, 6],
+            [4, 5, 6],
+            [4, 6, 7],
+            [5, 1, 2],
+            [5, 2, 6],
+            [0, 1, 5],
+            [0, 5, 4],
+        ]
+        .iter()
+        .map(|[a, b, c]| [c, b, a])
+        .flatten()
+        .copied()
+        .collect(),
+    );
     let mut counter = 0;
 
     event_loop.run(move |event, _, control_flow| {
         counter += 1;
         let rotation = (counter as f32) / 1000.0;
-
-        let mat1: Matrix4<f32> = Matrix4::new_translation(&Vector3::new(0.5, 0.0, 0.0))
+        let perspective = *Perspective3::new(1.0, 3.14 / 4.0, 0.1, 10.0).as_matrix();
+        let mat1: Matrix4<f32> = perspective
+            * Matrix4::new_translation(&Vector3::new(0.5, 0.0, -6.0))
             * Matrix4::from_euler_angles(rotation, 0.0, 0.0);
-        let mat2: Matrix4<f32> = Matrix4::new_translation(&Vector3::new(-0.5, 0.0, 0.0))
+        let mat2: Matrix4<f32> = perspective
+            * Matrix4::new_translation(&Vector3::new(-0.5, 0.0, -3.0))
             * Matrix4::from_euler_angles(-1.0 * rotation, 0.0, 0.0);
+        let cube_mat: Matrix4<f32> = perspective
+            * Matrix4::new_translation(&Vector3::new(0.0, 0.0, -2.0))
+            * Matrix4::new_scaling(0.1)
+            * Matrix4::from_euler_angles(0.0, -0.7 * rotation, 0.0);
         context.render_frame(&[
             (mesh, mat1.as_ptr() as *const std::ffi::c_void),
             (mesh, mat2.as_ptr() as *const std::ffi::c_void),
-            (mesh, mat1.as_ptr() as *const std::ffi::c_void),
+            (cube, cube_mat.as_ptr() as *const std::ffi::c_void),
         ]);
 
         match event {
