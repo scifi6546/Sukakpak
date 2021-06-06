007e0d10bc100f0c4b23ea62e5003de690ebfd31
Nicholas Alexeev <nickalexeev@protonmail.com>
2021-06-06 06:45:15 -0800

    working on api

--- sukakpak/src/lib.rs
+++ sukakpak/src/lib.rs
@@ -1,3 +1,4 @@
+use generational_arena::{Arena, Index as ArenaIndex};
 pub struct Context {}
 impl Context {
     pub fn new<R: Renderable>(mut render: R) -> ! {
@@ -13,13 +14,22 @@ impl Context {
 pub struct ContextChild<'a> {
     context: &'a mut Context,
 }
+pub struct Mesh {}
+pub struct Texture {}
+pub struct FrameBuffer {}
+//draws meshes. Will draw on update_uniform, bind_framebuffer, or force_draw
 impl<'a> ContextChild<'a> {
+    pub fn build_meshes() {}
+    pub fn build_texture() {}
     pub fn draw_mesh() {
         todo!()
     }
+    pub fn build_framebuffer() {}
     pub fn bind_framebuffer() {
         todo!()
     }
+    pub fn update_uniform() {}
+    pub fn force_draw() {}
 }
 /// User Provided code that provides draw calls
 pub trait Renderable {
