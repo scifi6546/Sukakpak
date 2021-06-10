18530f921ee8557195f72c0e8daa8fc91b3d72d6
Nicholas Alexeev <nickalexeev@protonmail.com>
2021-06-10 07:59:42 -0800

    working on allocators

--- sukakpak/src/backend/vertex_buffer.rs
+++ sukakpak/src/backend/vertex_buffer.rs
@@ -1,3 +1,4 @@
+mod allocators;
 pub struct VertexBufferPool {}
 impl VertexBufferPool {
     pub fn new(alloc_size: usize) -> Self {
@@ -5,3 +6,12 @@ impl VertexBufferPool {
     }
 }
 pub struct VertexBufferAllocation {}
+trait AllocTarget {}
+trait AllocateTarget {
+    type SubTarget;
+    /// allocates a block with a new size. If there is an existing block the data is copied over
+    /// from the old block to a bock with the new size
+    fn alloc_block(&mut self, size: usize);
+    /// gets an offset block
+    fn offset(&mut self, offset: usize) -> Self::SubTarget;
+}
