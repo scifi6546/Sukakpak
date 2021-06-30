31760d5087b67db215578cd19d76cd55a5e4bde8
Nick Alexeev <nickalexeev@protonmail.com>
2021-06-29 18:15:44 -0800

    reduced todos

---
+++ ass_bin/test_shaders/main/shader.frag
@@ -0,0 +1,8 @@
+#version 450
+//#extension GL_ARB_separate_shader_objects : enable
+layout(location=0) in vec2 uv;
+layout(location = 0) out vec4 outColor;
+layout(set=0,binding = 0) uniform sampler2D tex;
+void main(){
+	outColor = texture(tex,uv);
+}
--- ass_lib/src/lib.rs
+++ ass_lib/src/lib.rs
@@ -5,6 +5,8 @@ use std::{collections::HashMap, convert::TryFrom, fs::File, io::Read, path::Path
 pub struct Shader {
     vertex_shader: Module,
     vertex_info: ShaderDescription,
+    fragment_shader: Module,
+    fragment_info: ShaderDescription,
 }
 #[derive(Serialize, Deserialize, Debug)]
 pub struct AssembledSpirv {
@@ -68,7 +70,7 @@ impl TryFrom<Shader> for AssembledSpirv {
     fn try_from(shader: Shader) -> std::result::Result<Self, Self::Error> {
         Ok(AssembledSpirv {
             vertex_shader: Shader::get_module(shader.vertex_shader, shader.vertex_info)?,
-            fragment_shader: todo!(),
+            fragment_shader: Shader::get_module(shader.fragment_shader, shader.fragment_info)?,
             push_constants: todo!(),
             textures: todo!(),
         })
@@ -105,7 +107,7 @@ impl Shader {
                 .filter(|arg| arg.binding.is_some())
                 .map(|arg| {
                     (
-                        Self::naga_type_to_type(&module.types[arg.ty]),
+                        (&module.types[arg.ty]).into(),
                         Location {
                             location: match arg.binding.clone().expect("location found") {
                                 naga::Binding::Location { location, .. } => location,
@@ -124,19 +126,67 @@ impl Shader {
                 .clone(),
         })
     }
-    fn naga_type_to_type(ty: &naga::Type) -> Type {
-        todo!()
+}
+fn to_scalar(kind: naga::ScalarKind, size: naga::Bytes) -> ScalarType {
+    match kind {
+        naga::ScalarKind::Sint => todo!("not yet supported"),
+        naga::ScalarKind::Uint => todo!("not yet supported"),
+        naga::ScalarKind::Float => match size {
+            4 => ScalarType::F32,
+            8 => ScalarType::F64,
+            _ => panic!("invalid float size"),
+        },
+        naga::ScalarKind::Bool => todo!("not yet supported"),
     }
 }
-impl From<naga::Type> for Type {
-    fn from(ty: naga::Type) -> Self {
-        todo!()
+impl From<&naga::Type> for Type {
+    fn from(ty: &naga::Type) -> Self {
+        match ty.inner {
+            naga::TypeInner::Scalar { kind, width } => Type::Scalar(to_scalar(kind, width)),
+            naga::TypeInner::Vector { size, kind, width } => match size {
+                naga::VectorSize::Bi => Type::Vec2(to_scalar(kind, width)),
+                naga::VectorSize::Tri => Type::Vec3(to_scalar(kind, width)),
+                naga::VectorSize::Quad => Type::Vec4(to_scalar(kind, width)),
+            },
+            naga::TypeInner::Matrix {
+                columns,
+                rows,
+                width,
+            } => match columns {
+                naga::VectorSize::Bi => match rows {
+                    naga::VectorSize::Bi => match width {
+                        4 => Type::Mat2(ScalarType::F32),
+                        8 => Type::Mat2(ScalarType::F64),
+                        _ => panic!("unsupported floating point size"),
+                    },
+                    _ => panic!("only square matricies are supported"),
+                },
+                naga::VectorSize::Tri => match rows {
+                    naga::VectorSize::Tri => match width {
+                        4 => Type::Mat3(ScalarType::F32),
+                        8 => Type::Mat3(ScalarType::F64),
+                        _ => panic!("unsupported floating point size"),
+                    },
+                    _ => panic!("only square matricies are supported"),
+                },
+                naga::VectorSize::Quad => match rows {
+                    naga::VectorSize::Quad => match width {
+                        4 => Type::Mat4(ScalarType::F32),
+                        8 => Type::Mat4(ScalarType::F64),
+                        _ => panic!("unsupported floating point size"),
+                    },
+                    _ => panic!("only square matricies are supported"),
+                },
+            },
+            _ => todo!("type is currently unsupported"),
+        }
     }
 }
 use naga::{front::glsl, Module};
 #[derive(Deserialize, Debug)]
 pub struct ShaderConfig {
     vertex_shader: ModuleConfig,
+    fragment_shader: ModuleConfig,
 }
 #[derive(Deserialize, Debug)]
 pub struct ModuleConfig {
@@ -182,9 +232,38 @@ pub fn load_directory(path: &Path) -> Result<Shader> {
             defines: naga::FastHashMap::default(),
         },
     )?;
+    let fragment_shader = {
+        let mut file = File::open(path.join(config.fragment_shader.path))?;
+        let mut frag_str = String::new();
+        file.read_to_string(&mut frag_str)?;
+        let entry_points = [config.fragment_shader.entry_point]
+            .iter()
+            .map(|e| {
+                (
+                    e.name.clone(),
+                    match e.stage {
+                        ShaderStage::Fragment => naga::ShaderStage::Fragment,
+                        ShaderStage::Vertex => naga::ShaderStage::Vertex,
+                    },
+                )
+            })
+            .collect();
+        println!("{}", frag_str);
+        let s = glsl::parse_str(
+            &frag_str,
+            &glsl::Options {
+                entry_points,
+                defines: naga::FastHashMap::default(),
+            },
+        );
+        println!("{:#?}", s);
+        s?
+    };
     println!("{:#?}", vertex_shader);
     Ok(Shader {
         vertex_shader,
         vertex_info: config.vertex_shader.info,
+        fragment_shader,
+        fragment_info: config.fragment_shader.info,
     })
 }
