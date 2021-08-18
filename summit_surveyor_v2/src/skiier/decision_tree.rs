fa07e0f852109a336471c805b127635b6a0b2e9b
Nicholas Alexeev <nickalexeev@protonmail.com>
2021-08-18 07:56:21 -0800

    prototype

--- summit_surveyor_v2/src/skiier/decision_tree.rs
+++ summit_surveyor_v2/src/skiier/decision_tree.rs
@@ -1,7 +1,13 @@
 use super::*;
-pub trait Decision {
-    fn get_cost(&self, end: GraphNode) -> GraphWeight;
+struct DecisionCost {
+    cost: GraphWeight,
+    endpoint: GraphNode,
+}
+trait Decision {
+    fn get_cost(&self, start: GraphNode) -> DecisionCost;
 }
 //Decisons: Go up Lift, Go Down run to new start
 //To go up I need to know the end and start pos
 //To Go down the start and end are supplied
+// Two Types of decisions, path to go down (skiing, lifts) and destinations (lift bottoms,shops etc)
+// Decisons also have costs
